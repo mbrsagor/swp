56408ed5cdcdcd613864becc072f767bddba4209
mbrsagor <mbrsagor@gmail.com>
2022-06-01 00:48:38 +0600

    New: profile modles created.

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class DomainEntity(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Profile(DomainEntity):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userProfile')
+    name = models.CharField(max_length=50)
+    father_name = models.CharField(max_length=50)
+    mother_name = models.CharField(max_length=50)
+    board = models.CharField(max_length=100)
+    ssc_passing_year = models.DateField()
+    hsc_passing_year = models.DateField()
+    cgpa = models.DecimalField(max_digits=5, decimal_places=2, default=1.00)
+
+    def __str__(self):
+        return self.user.email
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile, created = Profile.objects.get_or_create(user=instance)
+        return profile
+
+
+post_save.connect(create_user_profile, sender=User)
