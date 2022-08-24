0b25da4836aac0c2455550edd1c7e7f3144e48d3
mohammadanarul <mdanarul7075@gmail.com>
2022-08-24 16:31:55 +0600

    unique id autometic generate remove.

--- users/signals.py
+++ users/signals.py
@@ -1,16 +1,8 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-from users.models import User, Student, Teacher, StudentProfile, TeacherProfile, AdminProfile
+from users.models import User, AdminProfile
 
 @receiver(post_save, sender=User)
 def create_student_profile(sender, instance, created, **kwargs):
     if created:
         AdminProfile.objects.create(user=instance)
-
-
-@receiver(pre_save, sender=StudentProfile)
-def uniqie_id_genarate(sender, instance, **kwargs):
-    if instance.roll_number:
-        year_last_two_digit = instance.created_at.strftime("%y")
-        instance.unique_id = f'075{year_last_two_digit}100{instance.program.code}{instance.roll_number}'
-
